8dcf232b9a4b49849ccad68b05a8597c8ddb0509
Agatha Rodrigues <agatharod@gmail.com>
2025-02-03 23:02:45 -0300

    Updates

---
+++ precache-manifest.ceb61e887c864431aa9c52574894f8e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848a67b66e8f9f0e7db5901667b43afd",
+    "url": "/tabacrow/index.html"
+  },
+  {
+    "revision": "7937bf0663aae8a976c8",
+    "url": "/tabacrow/static/css/main.d6d7b8c5.chunk.css"
+  },
+  {
+    "revision": "9d39abb2134e92f32505",
+    "url": "/tabacrow/static/js/2.16883420.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/tabacrow/static/js/2.16883420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d010ac102c0437f85a",
+    "url": "/tabacrow/static/js/3.08a75d23.chunk.js"
+  },
+  {
+    "revision": "7937bf0663aae8a976c8",
+    "url": "/tabacrow/static/js/main.81bf507e.chunk.js"
+  },
+  {
+    "revision": "09f5a500b04740b218f5",
+    "url": "/tabacrow/static/js/runtime-main.b96741b3.js"
+  }
+]);
